e1144c661cac0e7f7e3861869a6673ccc3424375
Bhrigu Yadav <bhriguyadav8210@gmail.com>
2024-03-12 10:37:49 +0530

    completed

---
+++ script.js
@@ -0,0 +1,8 @@
+gsap.from("#part-left,#part-right",{
+    y:-100,
+    duration:1,
+    delay:0.5,
+    stagger:1,
+    repeat:-1,
+
+})
---
+++ script3.js
@@ -0,0 +1,7 @@
+gsap.from("#nav",{
+    y:-100,
+    duration:2,
+    delay:0.5,
+    yoyo:true,
+    
+})
